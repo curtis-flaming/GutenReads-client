674d62d5626380f8dbd229f905f056a1a2cade46
curtis-flaming <curtisflaming@gmail.com>
2025-10-08 20:30:16 -0500

    button

---
+++ src/component-custom/button/button.tsx
@@ -0,0 +1,6 @@
+import { Button as ReactAriaButton } from "react-aria-components";
+
+// the one button to rule them all and in the darkness bind them
+export function Buttond() {
+  return <ReactAriaButton></ReactAriaButton>;
+}
